502e81988064fed958f7b8795d4c5f4db2dcd312
Nadthapon Sukeewadthana <38154439+teraflops-droid@users.noreply.github.com>
2022-07-25 11:10:28 +0000

    [WIP] Implement logic to find longest palindrome

--- Main.java
+++ Main.java
@@ -1,7 +1,18 @@
 import java.util.ArrayList;
 
-
 class Palindromic {
+    public static ArrayList<String> longest(ArrayList<String> palindrome){
+        ArrayList<String> temp = new ArrayList<>();
+        temp.add("0");
+        for(int i = 0; i< palindrome.size(); i++) {
+            String data = palindrome.get(i).toString();
+            if(data.length() >= temp.get(0).toString().length()) {
+                temp.add(data);
+            }
+        }
+        return temp;
+  
+    }
 
     public static void main(String[ ] args) {
         String input = "babad";
@@ -12,27 +23,40 @@ class Palindromic {
             char c = input.charAt(i);
             String flag = "" + c;
             normal.append(flag);
-            temp.append(flag);
-            System.out.println("First char " + normal);
-            for (int j = i+1; j < input.length(); j++) {
-                normal.append(input.charAt(j));
-                temp.append(input.charAt(j));
-                System.out.println("Next char " + normal);
-                StringBuilder reverse = temp.reverse();
-                System.out.println(reverse.toString());
-                String normalStr = normal.toString();
-                String reverseStr = reverse.toString();
-                System.out.println("Normal String: " + normalStr);
-                System.out.println("Reverse String: " + reverseStr);
-                if(normalStr.equals(reverseStr)) {
-                    System.out.print("Normal String: " + normalStr + " is equal to ");
-                    System.out.println("Reverse String: " + reverseStr);
-                    palindrome.add(normalStr);
-                } else {
-                    reverse.reverse();
-                }
-            }
+     
+
+          for (int j = i+1; j < iput.lengt
+            normal.append(i
+            temp.append(input.charAt(j));
+            System.out.println("Next char " + 
+          StringBuilder reverse = temp.reverse();
+          System.out.println(reverse.toString()
+          String normalStr = no
+          String reverseStr
+          System.out.print
+          System.out.pri 
+          if(normalStr.equals(reverseStr)) {
+              System . out.print("Normal String: " +
+            System.out.println("Reverse
+            palindrome.add(normalStr)
+        } else {
+            reverse.reverse();
         }
-        System.out.println(palindrome);
+        
+        
+        ut.println(palindrome);
+        
+         
+          
+          
+          
+        
+          
+        
+      
+    
+    
+    
+   System.out.println("Longest palindrome: " + longest(palindrome));
     }
 }
