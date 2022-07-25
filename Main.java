0fe5463789dcdbb9eee1c09099ffdcaf6fdcefe7
Nadthapon Sukeewadthana <38154439+teraflops-droid@users.noreply.github.com>
2022-07-25 12:32:10 +0000

    Refactor code

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 
 class Playground {
@@ -18,7 +19,9 @@ class Playground {
 
     public static void main(String[ ] args) {
 
-        String input = "cbbd";
+        // String input = "babad";
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
         ArrayList<String> palindrome = new ArrayList<String>();
         for (int i = 0; i < input.length(); i++) {
             StringBuilder normal = new StringBuilder();
@@ -27,26 +30,20 @@ class Playground {
             String flag = "" + c;
             normal.append(flag);
             temp.append(flag);
-            System.out.println("First char " + normal);
             for (int j = i+1; j < input.length(); j++) {
                 normal.append(input.charAt(j));
                 temp.append(input.charAt(j));
-                System.out.println("Next char " + normal);
                 StringBuilder reverse = temp.reverse();
-                System.out.println(reverse.toString());
                 String normalStr = normal.toString();
                 String reverseStr = reverse.toString();
-                System.out.println("Normal String: " + normalStr);
-                System.out.println("Reverse String: " + reverseStr);
                 if(normalStr.equals(reverseStr)) {
-                    System.out.print("Normal String: " + normalStr + " is equal to ");
-                    System.out.println("Reverse String: " + reverseStr);
                     palindrome.add(normalStr);
                 } else {
                     reverse.reverse();
                 }
             }
         }
+        sc.close();
         System.out.println(palindrome);
         System.out.println("Longest palindrome: " + getLongestString(palindrome));
     }
